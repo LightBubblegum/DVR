0856ddbcd7a9a8f03f46410d48a9d84dd94068c3
Egor Plotkin <luxoft.egor-plotkin.ext@valeo.com>
2019-08-12 12:04:46 +0300

    added simple test in main for n1616

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,33 @@
+/**
+ * autor: Egor Plotkin
+ */
+
 #include <iostream>
-#include <array>
-#include <experimental/array>
-#include <type_traits>
-#include <cassert>
-#include <iomanip>
-#include <bitset>
+#include <unistd.h>
+#include "spfd54124b.h"
+
+using namespace std;
+
+#define RST_PIN     (21)
+#define CS_PIN      (22)
+#define MOSI_PIN    (12)
+#define SCLK_PIN    (14)
 
 int main(int argc, char *argv[])
 {
+    cout << "Programm working" << endl;
+
+    LcdEq::NOKIA1616 n1616(RST_PIN, CS_PIN, MOSI_PIN, SCLK_PIN);
+    n1616.init();
+
+    while(1)
+    {
+        for(rgb_color16bit color = 0; color < 0xFFF0; color += 10)
+        {
+            usleep(10000);
+            n1616.fill_background(color);
+        }
+    }
+
 	return 0;
 }
